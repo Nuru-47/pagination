f3aeb3a49d151f8f92bec4ba832926009936b9fe
Nuru-47 <nurulddinmulla@gmail.com>
2024-07-05 15:48:09 +0530

    Fourth Commit

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Loading.css';
 
-const Loading = () => {
+const Loading = ({ loading }) => {
     return (
         <div className="loading-spinner">
             <p>Loading...</p>
-            {/* Add your loading animation or spinner component here */}
+            {loading && <div className="loading-overlay"></div>}
         </div>
     );
 };
