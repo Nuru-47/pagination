b4ceec0e9d4e4d6df94567f2ef1471f21010b980
Nuru-47 <nurulddinmulla@gmail.com>
2024-07-05 11:46:24 +0530

    Third Commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -181,7 +181,7 @@ alt="logo"
          <div className="modal-content">
     <div className="modal-header">
      <h5 className="modal-title" id="updateCandidateModalLabel">Update Candidate</h5>
-   <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={handleCloseUpdateModal}></button>
+   
      </div>
     <div className="modal-body">
 <form onSubmit={handleSubmit}>
